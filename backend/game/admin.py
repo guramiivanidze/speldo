e7788fd492a4d75ba0f7a7c2b795bfc517a93769
guramiivanidze <gurami.ivanidze.96@gmail.com>
2026-03-01 19:24:44 +0400

    upd

--- backend/game/admin.py
+++ backend/game/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.shortcuts import render, redirect
+from django.urls import path
+from django.contrib import messages
 from import_export import resources, fields
 from import_export.admin import ImportExportModelAdmin
 from .models import Game, GamePlayer, DevelopmentCard, Noble
@@ -39,6 +42,7 @@ class DevelopmentCardAdmin(ImportExportModelAdmin):
     ordering = ['level', 'bonus', 'id']
     search_fields = ['bonus']
     readonly_fields = ['image_preview']
+    actions = ['bulk_upload_images']
     
     fieldsets = (
         ('Card Info', {
@@ -51,6 +55,48 @@ class DevelopmentCardAdmin(ImportExportModelAdmin):
             'fields': ('background_image', 'image_preview'),
         }),
     )
+    
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('bulk-upload-images/', self.admin_site.admin_view(self.bulk_upload_view), name='game_developmentcard_bulk_upload'),
+        ]
+        return custom_urls + urls
+    
+    @admin.action(description='Bulk upload images for selected cards')
+    def bulk_upload_images(self, request, queryset):
+        # Store selected card IDs in session
+        selected_ids = list(queryset.values_list('id', flat=True))
+        request.session['bulk_upload_card_ids'] = selected_ids
+        return redirect('admin:game_developmentcard_bulk_upload')
+    
+    def bulk_upload_view(self, request):
+        card_ids = request.session.get('bulk_upload_card_ids', [])
+        cards = DevelopmentCard.objects.filter(id__in=card_ids).order_by('level', 'bonus', 'id')
+        
+        if request.method == 'POST':
+            uploaded_count = 0
+            for card in cards:
+                file_key = f'image_{card.id}'
+                if file_key in request.FILES:
+                    card.background_image = request.FILES[file_key]
+                    card.save()
+                    uploaded_count += 1
+            
+            clear_card_cache()
+            messages.success(request, f'Successfully uploaded {uploaded_count} images.')
+            # Clear session
+            if 'bulk_upload_card_ids' in request.session:
+                del request.session['bulk_upload_card_ids']
+            return redirect('admin:game_developmentcard_changelist')
+        
+        context = {
+            'title': 'Bulk Upload Card Images',
+            'cards': cards,
+            'opts': self.model._meta,
+            'has_view_permission': True,
+        }
+        return render(request, 'admin/game/developmentcard/bulk_upload.html', context)
     
     def image_preview_small(self, obj):
         if obj.background_image:
