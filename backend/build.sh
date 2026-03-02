416e7164935421bc271205e2f15689a883c8f79a
Gurami Ivanidze <guiv01@betsson.ge>
2026-03-02 14:53:12 +0400

    upd

--- backend/build.sh
+++ backend/build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py migrate
+
+
+python manage.py createsuperuser --no-input --username superuser --email superuser@example.com --password Skullbusher123
