21cefc41aec4caa1238e39e0c61e9bface50db7d
Gurami Ivanidze <guiv01@betsson.ge>
2026-03-05 13:30:33 +0400

    upd

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import FriendRequest, Friendship
 
-# Register your models here.
+
+@admin.register(FriendRequest)
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'status', 'created_at', 'responded_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('from_user__username', 'to_user__username')
+    raw_id_fields = ('from_user', 'to_user')
+    ordering = ('-created_at',)
+
+
+@admin.register(Friendship)
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ('user', 'friend', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username', 'friend__username')
+    raw_id_fields = ('user', 'friend')
+    ordering = ('-created_at',)
